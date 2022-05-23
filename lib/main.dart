c08c0adb8c3886028e68f01f687a2aa4ea76ac0c
techner-sachin <sachin@technersolutions.com>
2022-05-23 18:39:47 +0530

    space

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,50 +18,41 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
-  final List<Transaction>transaction=[
-    Transaction(id: 'FT1', title: 'Bag', amount: 100.25, date:DateTime.now()),
-    Transaction(id: 'FT1', title: 'food', amount: 78.25, date:DateTime.now())
+  final List<Transaction> transaction = [
+    Transaction(id: 'FT1', title: 'Bag', amount: 100.25, date: DateTime.now()),
+    Transaction(id: 'FT1', title: 'food', amount: 78.25, date: DateTime.now())
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const  Text("New App"),
-        centerTitle: true
-        ,
+        title: const Text("New App"),
+        centerTitle: true,
       ),
-
-      body: Column(children:  [
-
-       const Card(
-
-          child: Text("Chart"),
-        ),
-        const Card(
-          child: Text("Transactions"),
-        ),
-          Column(
-              children:transaction.map((ft) {
-                return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      child: Text(ft.amount.toString()),
-                    ),
-                    Column(
-                      children: [
-
-                        Text(ft.title),
-                        Text(ft.date.toString())
-
-                      ],
-                    )
-                 ],
-               ),
-            );}).toList()
+      body: Column(
+        children: [
+          const Card(
+            child: Text("Chart"),
           ),
+          const Card(
+            child: Text("Transactions"),
+          ),
+          Column(
+              children: transaction.map((ft) {
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    child: Text(ft.amount.toString()),
+                  ),
+                  Column(
+                    children: [Text(ft.title), Text(ft.date.toString())],
+                  )
+                ],
+              ),
+            );
+          }).toList()),
         ],
       ),
     );
